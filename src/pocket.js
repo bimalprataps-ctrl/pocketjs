5fb7ef84952b81723a68439f5b378424fac636e0
Bimal Pratap Shah <Bimalprataps@gmail.com>
2026-05-18 13:33:14 +0545

    Add bottom sheet export

--- src/pocket.js
+++ src/pocket.js
@@ -2,12 +2,14 @@ import { createToast } from './components/toast.js'
 import { createModal } from './components/modal.js'
 import { createTabBar } from './components/tab-bar.js'
 import { createSwipeCards } from './components/swipe-cards.js'
+import { createBottomSheet } from './components/bottom-sheet.js'
 
 export const Pocket = {
   createToast,
   createModal,
   createTabBar,
-  createSwipeCards
+  createSwipeCards,
+  createBottomSheet
 }
 
 if (typeof window !== 'undefined') {
