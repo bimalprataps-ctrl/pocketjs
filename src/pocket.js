ee86636824ef83c6d0a835b7b3ce14dc731754f2
Bimal Pratap Shah <Bimalprataps@gmail.com>
2026-05-19 11:58:39 +0545

    Standardize Pocket framework exports

--- src/pocket.js
+++ src/pocket.js
@@ -1,26 +1,43 @@
-import { createToast } from './components/toast.js'
-import { createModal } from './components/modal.js'
-import { createTabBar } from './components/tab-bar.js'
-import { createSwipeCards } from './components/swipe-cards.js'
-import { createBottomSheet } from './components/bottom-sheet.js'
+import { createActionSheet } from './components/action-sheet.js';
+import { createBottomSheet } from './components/bottom-sheet.js';
+import { createModal } from './components/modal.js';
+import { createPullToRefresh } from './components/pull-refresh.js';
+import { createSwipeCards } from './components/swipe-cards.js';
+import { createTabBar } from './components/tab-bar.js';
+import { createTabs } from './components/tabs.js';
+import { createToast } from './components/toast.js';
+
+const version = '0.1.0';
 
 const Pocket = {
-  createToast,
+  version,
+
+  createActionSheet,
+  createBottomSheet,
   createModal,
-  createTabBar,
+  createPullToRefresh,
   createSwipeCards,
-  createBottomSheet
-}
+  createTabBar,
+  createTabs,
+  createToast
+};
 
 export {
+  version,
   Pocket,
-  createToast,
+
+  createActionSheet,
+  createBottomSheet,
   createModal,
-  createTabBar,
+  createPullToRefresh,
   createSwipeCards,
-  createBottomSheet
-}
+  createTabBar,
+  createTabs,
+  createToast
+};
+
+export default Pocket;
 
 if (typeof window !== 'undefined') {
-  window.Pocket = Pocket
+  window.Pocket = Pocket;
 }
