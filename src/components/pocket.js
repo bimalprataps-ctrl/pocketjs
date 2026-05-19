f3cd57cacd79ffd36b0bb3240e57099a0582bd78
Bimal Pratap Shah <Bimalprataps@gmail.com>
2026-05-19 11:56:51 +0545

    Standardize Pocket global exports

--- src/components/pocket.js
+++ src/components/pocket.js
@@ -1,6 +1,34 @@
-pocket.js
-window.Pocket = {
-  hello() {
-    alert('PocketJS CDN works')
-  }
+import { createActionSheet } from './action-sheet.js';
+import { createBottomSheet } from './bottom-sheet.js';
+import { createModal } from './modal.js';
+import { createPullToRefresh } from './pull-refresh.js';
+import { createSwipeCards } from './swipe-cards.js';
+import { createTabBar } from './tab-bar.js';
+import { createTabs } from './tabs.js';
+import { createToast } from './toast.js';
+
+export {
+  createActionSheet,
+  createBottomSheet,
+  createModal,
+  createPullToRefresh,
+  createSwipeCards,
+  createTabBar,
+  createTabs,
+  createToast
+};
+
+export const Pocket = {
+  createActionSheet,
+  createBottomSheet,
+  createModal,
+  createPullToRefresh,
+  createSwipeCards,
+  createTabBar,
+  createTabs,
+  createToast
+};
+
+if (typeof window !== 'undefined') {
+  window.Pocket = Pocket;
 }
